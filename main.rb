c8a0007c357b704d584837baf219b7860b016409
Blake Ruddock <ruddock.blake@gmail.com>
2013-10-08 12:02:34 -0400

    Setup environment

---
+++ main.rb
@@ -0,0 +1,20 @@
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'pry'
+require 'twitter'
+require 'dotenv'
+require 'stock_quote'
+
+
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+
+Dotenv.load
+
+@twitter = Twitter::Streaming::Client.new do |config|
+  config.consumer_key        = ENV["CONSUMER_KEY"]
+  config.consumer_secret    = ENV["CONSUMER_SECRET"]
+  config.access_token        = ENV["ACCESS_TOKEN"]
+  config.access_token_secret = ENV["ACCESS_SECRET"]
+end
+
+
