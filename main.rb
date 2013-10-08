247f0de5c93ba2f174f1e3f94968aed306e81d15
Blake Ruddock <ruddock.blake@gmail.com>
2013-10-08 13:31:05 -0400

    finished exercise without styling

--- main.rb
+++ main.rb
@@ -1,20 +1,35 @@
+require 'pry'
 require 'sinatra'
 require 'sinatra/reloader' if development?
-require 'pry'
 require 'twitter'
 require 'dotenv'
 require 'stock_quote'
+set :server, 'webrick'
 
 
 OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
 Dotenv.load
 
-@twitter = Twitter::Streaming::Client.new do |config|
+$twitter = Twitter::REST::Client.new do |config|
   config.consumer_key        = ENV["CONSUMER_KEY"]
   config.consumer_secret    = ENV["CONSUMER_SECRET"]
   config.access_token        = ENV["ACCESS_TOKEN"]
   config.access_token_secret = ENV["ACCESS_SECRET"]
 end
 
+get '/:stock' do
+	@symbol = params[:stock]
+	@company = StockQuote::Stock.quote(@symbol).company
+	@high = StockQuote::Stock.quote(@symbol).high
+	@low = StockQuote::Stock.quote(@symbol).low
+	@price = StockQuote::Stock.quote(@symbol).last
+	@volume = StockQuote::Stock.quote(@symbol).volume
+	@tweets = $twitter.search("$#{@symbol}", :count => 10, :result_type => "recent").collect do |tweet|
+  		if tweet.lang == 'en'
+  			"#{tweet.user.screen_name}: #{tweet.text}"
+  		end
+	end
 
+	erb :stock
+end
