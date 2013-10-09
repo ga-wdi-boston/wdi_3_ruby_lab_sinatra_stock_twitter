6103550c13496f26dfcc2285d46128c7d74555d6
Dan Crews <dancrews@me.com>
2013-10-08 20:52:21 -0400

    Implemented basic capability without formating

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'https://rubygems.org'
+
+gem 'sinatra'
+gem 'sinatra-reloader'
+gem 'pry'
+gem 'twitter', '5.0.0.rc.1', github: 'sferik/twitter'
+gem 'stock_quote', github: 'tyrauber/stock_quote'
+gem 'dotenv'
+gem 'multi_json'
---
+++ main.rb
@@ -0,0 +1,64 @@
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'pry'
+require 'twitter'
+require 'multi_json'
+require 'stock_quote'
+require 'dotenv'
+
+set :server, 'webrick'
+
+Dotenv.load
+
+client = Twitter::REST::Client.new do |config|
+	config.consumer_key = 				ENV["CONSUMER_KEY"]
+	config.consumer_secret = 			ENV["CONSUMER_SECRET"]
+	config.access_token = 				ENV["ACCESS_TOKEN"]
+	config.access_token_secret = 	ENV["ACCESS_SECRET"]
+end
+
+# show the stock form
+get '/stock/new' do
+	erb :stock_form
+end
+
+post '/stock/create' do
+	stock_name = params[:stock_name]
+	@stock = StockQuote::Stock.quote(stock_name)
+	# @stock_current = "$#{StockQuote::Stock.quote("#{stock_name}").last}"
+	erb :show_stock
+end
+
+get '/stock/:symbol' do
+@symbol = params[:symbol]
+
+stock = StockQuote::Stock.quote(@symbol)
+
+
+	@company = stock.company
+	@stock_last = stock.last
+	@daily_open = stock.open
+	@daily_close = stock.y_close
+	@high = stock.high
+	
+
+	# @stock_last = 			StockQuote::Stock.quote(@symbol).last
+	# @daily_open = 			StockQuote::Stock.quote(@symbol).open
+	# @daily_close	=			StockQuote::Stock.quote(@symbol).y_close}
+	# @high = 						StockQuote::Stock.quote(@symbol).high}
+
+# binding.pry
+
+ @recent_tweets = client.search(@symbol, :count => 25, :result_type => "recent", :lang => "en").collect do |tweet|
+  "#{tweet.text}"
+
+end
+
+	  return erb :stocks
+
+
+	# @recent_tweets = client.search("#{@symbol}", :count => 50, :result_type => "recent").collect do |tweet|
+ #  "#{tweet.user.screen_name}: #{tweet.text}"
+
+	
+end
