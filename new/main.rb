c2d065cd14ffb6b1f4cc4901ea51a2d0756ca92f
Ryan Page <rpage326@gmail.com>
2013-10-08 22:45:58 -0400

    Made progress. All folders done. Not running correctly yet.

--- new/main.rb
+++ new/main.rb
@@ -4,37 +4,37 @@ require 'dotenv'
 require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'stock_quote'
-
 set :server, 'webrick'
-
-Dotenv.load
-
 
 OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
-client = Twitter::REST::Client.new do |config|
+Dotenv.load
+
+$twitter = Twitter::REST::Client.new do |config|
   config.consumer_key        = ENV["CONSUMER_KEY"]
   config.consumer_secret    = ENV["CONSUMER_SECRET"]
   config.access_token        = ENV["ACCESS_TOKEN"]
   config.access_token_secret = ENV["ACCESS_SECRET"]
-
 end
 
-stock = "GOOG"
+# stock = "GOOG"
 
 
 
-user_input = gets.chomp
+get '/:stock' do
+	@symbol = params[:stock]
+	@company = StockQuote::Stock.quote(@symbol).company
+	@high = StockQuote::Stock.quote(@symbol).high
+	@low = StockQuote::Stock.quote(@symbol).low
+	@price = StockQuote::Stock.quote(@symbol).last
+	@volume = StockQuote::Stock.quote(@symbol).volume
+	@tweets = $twitter.search("$#{@symbol}", :count => 25, :result_type => "recent").collect do |tweet|
+  		if tweet.lang == 'en'
+  			"#{tweet.user.screen_name}: #{tweet.text}"
+  		end
+	end
 
-@stockquote = StockQuote::Stock.quote(stock)
-@tweets = client.search(stock, :count => 25, :result_type => "recent", :lang => "en").collect"#{tweet.text}"
-
-binding.pry
-
-topics = gets.chomp
-get 'stocks/:stock' do
-
+	erb :stock
 end
-
 
 
